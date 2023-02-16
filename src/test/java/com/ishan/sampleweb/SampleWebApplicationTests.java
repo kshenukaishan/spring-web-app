19c259d5c18e50a84528acdd5631fe7cbc1d5856
kshenukaishan <kshenukaishan@outlook.com>
2023-02-16 21:59:12 +0530

    hava initialized with two models

---
+++ src/test/java/com/ishan/sampleweb/SampleWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ishan.sampleweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
